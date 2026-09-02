7fcc3c5ec203b010053ff64d966092d180074647
manuela070380-web <manuela070380@gmail.com>
2026-09-02 20:12:39 -0300

    batatinha didatico

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,12 +1,24 @@
 import {Link} from 'react-router'
-
+import { useState } from 'react'
 function Auth(){
+const [batatinha, setBatatinha] = useState(0);
+
+function sub(){
+    setBatatinha(batatinha - 1)
+}
+
+
 return(
 
 <>
     <div class="h-full flex items-center">
-        <div class="max-w-sm mx-auto my-auto p-5 bg-secondary text-white rounded-lg shadow-md flex flex-col">
-            <Link to="/" class="mb-5 shadow-inner">Voltar</Link>
+        <div class="max-w-sm mx-auto my-auto p-5 bg-secondary text-primary rounded-lg shadow-md flex flex-col">
+            <Link to="/" class="mb-5 text-primary">Voltar</Link>
+             {batatinha}
+
+            <div className="bg-red-100 rounded-full p-2" onClick={sub}>-</div>
+            {batatinha}
+            <div className="bg-green-100 rounded-full p-2" onClick={() => setBatatinha(batatinha + 1)}>+</div>
 
             <form class="flex flex-col">
                 Email: <input id="iEmailLogin" type="email" placeholder="Digite o seu email cadastro" />
